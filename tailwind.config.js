4770aed8429e024609e364bba8c9b9c0822bfa44
jeromeborg <contact@jeromeb.org>
2021-03-09 10:48:01 +0100

    quick styles for deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
+// CUSTOMIZED COLOR
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     "./src/**/*.jsx", "./src/**/*.js", "./src/**/*.html"
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+        teal: colors.teal,
+        orange: colors.orange
+    },
   },
   variants: {
     extend: {},
